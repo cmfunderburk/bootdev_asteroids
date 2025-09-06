69c769d880bcacb2bd4072690265611d27f0826b
cmfunderburk <cmfunderburk@gmail.com>
2025-09-06 16:26:22 -0500

    not working yet

--- main.py
+++ main.py
@@ -3,23 +3,41 @@
 # throughout this file
 import pygame
 from constants import *
+from circleshape import *
+from player import *
 
 def main():
+    # Print welcome messages
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    # Initialize pygame
     pygame.init()
 
+
+    # Set up the display
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    # FPS control
+    clock = pygame.time.Clock()
+    dt = 0
+
+    # Initialize player
+    Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    # Main loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
         screen.fill(color="black")
+        player.draw(screen)
         pygame.display.flip()
+
+        # Loop FPS control
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
